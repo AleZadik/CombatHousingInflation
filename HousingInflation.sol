106289ea1b5d6a686535fc9603db4fe73e44fc3b
AleZadik <awzadik@gmail.com>
2022-11-16 04:18:07 -0300

    Estate struct + mapping + createEstate to track

--- HousingInflation.sol
+++ HousingInflation.sol
@@ -12,4 +12,23 @@ contract HouseInflation {
     function requestYoyInflation() public {
         truflationTester.requestYoyInflation();
     }
+
+    struct Estate {
+        string name;
+        address owner;
+        uint256 initialPrice; // Price in USD at time of creation
+        uint256 currentPrice;
+        string metadataURI; // OPTIONAL
+        string status; // SALE, SOLD, RENT, RENTED, OFF
+        uint256 creationDate; // TS in secs
+        string location; // address str
+    }
+    uint256 public estateCount = 0;
+    mapping(uint256 => Estate) public estates;
+    uint256[] public estateIds;
+
+    function createEstate(string memory _name, uint256 _initialPrice, string memory _metadataURI, string memory _location) public {
+        estates[estateCount++] = Estate(_name, msg.sender, _initialPrice, _initialPrice, _metadataURI, "OFF", block.timestamp, _location);
+        estateIds.push(estateCount);
+    }
 }
