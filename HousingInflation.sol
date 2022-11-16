62176c75905dcfdd0f8992e50920aa6ac4a37acc
AleZadik <awzadik@gmail.com>
2022-11-16 04:39:15 -0300

    Create Users, attach estate to them and list

--- HousingInflation.sol
+++ HousingInflation.sol
@@ -28,7 +28,32 @@ contract HouseInflation {
     uint256[] public estateIds;
 
     function createEstate(string memory _name, uint256 _initialPrice, string memory _metadataURI, string memory _location) public {
-        estates[estateCount++] = Estate(_name, msg.sender, _initialPrice, _initialPrice, _metadataURI, "OFF", block.timestamp, _location);
+        if(userByAddress[msg.sender] == 0) {
+            createUser(msg.sender);
+            emit UserCreated(msg.sender);
+        }
+        estateCount++;
+        uint256 ts = block.timestamp;
+        estates[estateCount] = Estate(_name, msg.sender, _initialPrice, _initialPrice, _metadataURI, "OFF", ts, _location);
         estateIds.push(estateCount);
+        users[userByAddress[msg.sender]].estates.push(estateCount);
+        emit EstateCreated(_name, msg.sender, _initialPrice, _metadataURI, "OFF", ts, _location);
     }
+    struct User {
+        address wallet;
+        uint256[] estateIds;
+    }
+    uint256 public userCount = 0;
+    uint256[] public userIds;
+    mapping(uint256 => User) public users;
+    mapping(address => uint256) public userByAddress;
+
+    function createUser(address _wallet) public {
+        users[userCount++] = User(_wallet, new uint256[](0));
+        userIds.push(userCount);
+    }
+
+    // emit events
+    event EstateCreated(string name, address owner, uint256 initialPrice, string metadataURI, string status, uint256 creationDate, string location);
+    event UserCreated(address wallet);
 }
